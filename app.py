006f5e357a2649ce46a67f0e3db14d8da53e679d
bradyrpeterson <bradyrpeterson2@gmail.com>
2025-10-27 05:28:59 +0000

    Updated HTML and CSS to basically incorporate logos

--- app.py
+++ app.py
@@ -10,24 +10,34 @@ app = Flask(__name__)
 with open("fbs_teams_2025.json","r") as f:
     fbs_teams=json.load(f)
 
+#Get the team logos from the other json file
+with open("team_logos.json", "r") as f:
+    team_logos = json.load(f)
+
 @app.route("/", methods=["GET", "POST"])
 def home():
+    #New with logos - Instantiate all the variables first
     result = None
+    home_team= None
+    away_team = None
+    home_logo = None
+    away_logo = None
     if request.method == "POST":
         home_team = request.form["home_team"]
         away_team = request.form["away_team"]
 
-        try:
-            margin, prob = predict_game(home_team, away_team)
-            winner = home_team if margin > 0 else away_team
-            if(margin>0):
-                winner_prob=prob
-            else: winner_prob=1-prob
-            result = f"{winner} has a {winner_prob*100:.2f}% chance to win and is predicted to win by {abs(margin):.2f}"
-        except KeyError:
-            result = "One of those teams isn't available in the dataset."
+        
+        margin, prob = predict_game(home_team, away_team)
+        winner = home_team if margin > 0 else away_team
+        if(margin>0):
+             winner_prob=prob
+        else: winner_prob=1-prob
+        result = f"{winner} has a {winner_prob*100:.2f}% chance to win and is predicted to win by {abs(margin):.2f}"
+        
+        home_logo = team_logos.get(home_team)
+        away_logo = team_logos.get(away_team)
 
-    return render_template("index.html", teams=fbs_teams, result=result)
+    return render_template("index.html", teams=fbs_teams, result=result,home_team=home_team,away_team=away_team,home_logo=home_logo,away_logo=away_logo)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
